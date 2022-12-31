92cb4fb505f6bb44fbff770fa93ce10a12771446
Andre0n <andreon098@gmail.com>
2022-12-31 14:34:59 -0300

    Add `FPS` system to game

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ mod paddle;
 use bevy::core_pipeline::clear_color::ClearColorConfig;
 use bevy::prelude::*;
 use paddle::{spawn_paddle, PaddleLocation};
+use bevy::time::FixedTimestep;
 
 // Window defaults
 const WINDOW_WIDTH: f32 = 854.;
 const WINDOW_HEIGHT: f32 = 480.;
 
+// Defines the amount of time that should elapse between each physics step. (FPS)
+const TIME_STEP: f32 = 1.0 / 60.0;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -20,6 +23,7 @@ fn main() {
             ..default()
         }))
         .add_startup_system(setup)
+        .add_system_set(SystemSet::new().with_run_criteria(FixedTimestep::step(TIME_STEP as f64)))
         .run();
 }
 
