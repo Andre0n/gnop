58f7b7da2c69f517013c19f6d4c458d99615baf7
Andre0n <andreon098@gmail.com>
2022-12-31 12:41:53 -0300

    Add `WindowDescriptor`: game window related constants and settings

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use bevy::prelude::*;
 
-fn main() {
-    App::new().run();
-}
+// Window defaults
+const WINDOW_WIDTH: f32 = 854.;
+const WINDOW_HEIGHT: f32 = 480.;
 
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: WINDOW_WIDTH,
+                height: WINDOW_HEIGHT,
+                title: "GNOP - The Rust Pong".to_string(),
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
+        .run();
+}
