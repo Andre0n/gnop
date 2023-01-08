57e0aff0ebdd1e6d56a04d67d83fd3f2a76bfcc1
Andre0n <andreon098@gmail.com>
2023-01-08 10:00:35 -0300

    Add `ResetGameEvent` to game

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,9 @@ struct Collider;
 #[derive(Default)]
 struct CollisionEvent;
 
+#[derive(Default)]
+struct ResetGameEvent;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -42,6 +45,7 @@ fn main() {
         .add_startup_system(setup)
         .add_event::<CollisionEvent>()
         .add_event::<ResetBallEvent>()
+        .add_event::<ResetGameEvent>()
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
